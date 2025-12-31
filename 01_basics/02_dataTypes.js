33026da2009832e2883cb0aead95250cbb884d96
VanshBamborde <vanshbamborde882005@gmail.com>
2025-12-31 17:12:05 +0000

    new changes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,1 +1,26 @@
 // dataTypes are very very important
+"user strict"; // treat all js code as newer version
+
+// alert(3 + 3) we are using nodejs, not browser
+
+let name = "Vansh" // string
+let age = 20; // number
+
+// Primitive dataTypes
+// number - 2 to power 53
+// bigint
+// string - ""
+// boolean - true/false
+// null - standalone value
+// undefined - let a;
+// symbol - unique
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object IMP for interview
+ 
+
+
