acb71d645cbe5d986d3b1c647101293f664fb120
VanshBamborde <vanshbamborde882005@gmail.com>
2025-12-31 17:35:35 +0000

    new changes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33abcd";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof (valueInNumber)); // number for "33"
+// console.log(valueInNumber); // NaN - Not a Number for "33abcd"
+
+
+//notes
+// "33" => 33 
+// "33abc" => NaN - Not a Number
+// true - 1, false - 0;
+
+
+let isLoggedIn = "vansh";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); // true - let isLoggedIn = 1;
+// console.log(booleanIsLoggedIn); //false - let isLoggedIn = "";
+// console.log(booleanIsLoggedIn); // true - let isLoggedIn = "vansh";
+
+let someNumber = 68;
+let stringNum = String(someNumber);
+console.log(stringNum); // output - 68
+console.log(typeof stringNum); // output - string
+
