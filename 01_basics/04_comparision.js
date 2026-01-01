e173fa88f574b593f3f0c43c36757d76df5ac799
VanshBamborde <vanshbamborde882005@gmail.com>
2026-01-01 15:16:20 +0000

    new changes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// We avoid these type of comparision
+
+// console.log("2" > 1);
+// console.log("02" < 1);
+
+console.log(null > 0); // false
+console.log(null < 0); // false
+console.log(null >= 0); // true
+
+console.log(undefined == 0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+
+// strict check
+// ===
+
+console.log("2" === 2);; // false // it will check value as well as dataType
+
+
