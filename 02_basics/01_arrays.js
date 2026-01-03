454271094facebc7eaa1d38c3d70724e3b197fc9
VanshBamborde <vanshbamborde882005@gmail.com>
2026-01-03 10:03:15 +0000

    new changes

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -5,7 +5,7 @@ const myArray = [1,2,3,4,5];
 const myHeros = ["Ironman","Hulk"];
 const myArray2 = new Array(4,2,3,5);// Another way of representing the array
 
-console.log(myArray[0]);
+// console.log(myArray[0]);
 
 // ************** Array Methods *******************
 
@@ -16,11 +16,37 @@ console.log(myArray[0]);
 
 myArray.unshift(9); // it will add at the start
 myArray.shift(); // it will remove from the start
-console.log(myArray);
+// console.log(myArray);
 
 
 // it will true or false value
-const result = myArray.includes(9);
-console.log(result);
+// const result = myArray.includes(9);
+// console.log(result);
 
-console.log(myArray.indexOf(3));
+// console.log(myArray.indexOf(3));
+
+// join convert array into string
+const newArray = myArray.join();
+// console.log(myArray);
+// console.log(newArray);
+// console.log(typeof newArray);//string
+
+// slice and spilce
+
+console.log("A ", myArray);
+
+// it doest not include the last element
+const myn1 = myArray.slice(1,3);
+console.log(myn1);
+console.log("B ", myArray);
+
+// it does include the start and last element but when we do 'splice' then it will cut that portion from the original array
+const myn2 = myArray.splice(1,3);
+console.log("C ", myArray);
+console.log(myn2);
+
+// original array
+console.log(myArray); // only [1, 5]
+
+
+
