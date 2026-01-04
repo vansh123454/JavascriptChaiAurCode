1c3a601bae915b40ebb2328f70261f9f4be72395
VanshBamborde <vanshbamborde882005@gmail.com>
2026-01-04 15:05:55 +0000

    object passing into the function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -47,12 +47,53 @@ function loginUserMessage(username = "xyz"){
 // console.log(res); // vansh just logged in
 
 // without if conditon
-console.log(loginUserMessage()); // undefined just logged in
+// console.log(loginUserMessage()); // undefined just logged in
 
 //with if condition and we did not add default check
 // console.log(loginUserMessage()); // Please enter the username
 
 // with default name check 
-console.log(loginUserMessage()); // xyz just logged in
+// console.log(loginUserMessage()); // xyz just logged in
+
+// ************ Cart ***************************
+
+// function addCart(new1){
+//     return new1;
+// }
+// // const res = addCart(200); // 200
+
+// this is rest operator. Spread operator and rest operator are similar
+// function addCart(...new1){
+//     return new1;
+// }
+// const res = addCart(200,400,500); // [ 200, 400, 500 ]
+
+
+function addCart(value1, value2, ...new1){
+    return new1;
+}
+// 200 goes to value1, 400 goes to value2 and rest goes to new1
+const res = addCart(200,400,500,100,300); // [ 500, 100, 300 ]
+console.log(res);
+
+// object passing in function
+
+const user = {
+    username: "vansh",
+    age: 20,
+}
+
+function handleObject(anyObject){
+    console.log(`My name is ${anyObject.username} and i am ${anyObject.age} years old`)
+}
+
+handleObject(user); // My name is vansh and i am 20 years old
+
+// we can directly pass object 
+handleObject({
+    username: "once",
+    age: 21,
+}); // My name is once and i am 21 years old
+
 
 
